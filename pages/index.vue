361fb1b99578d6726a868e68007248a535e5b69c
RaySandhu <aryamansandhu123@gmail.com>
2024-02-20 14:50:19 -0800

    Sample simple API call

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,17 @@
         <h2 style="background-color: blue;">
             Index Content
         </h2>
-        <v-btn>
-            Problematic V button
+        <v-btn @click="printData()">
+            Example API call
         </v-btn>
     </div>
 </template>
 
 <script setup lang="ts">
+const { data } = await useFetch('/api/example')
+
+const printData = () => {
+    console.log(data.value)
+}
 
 </script>
