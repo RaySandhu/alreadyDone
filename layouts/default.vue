193f9c5ed9731ba917bf0aed4bce4c2fed14e906
Ekkiddle <ekkiddle@gmail.com>
2024-02-28 23:30:44 -0700

    Added layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,13 @@
 <template>
-    <Navbar />
-    <slot />
+    <div> 
+        <Navbar />
+        <div class="flex"> 
+            <Sidebar />
+            <div class="overflow-y-auto mt-16">
+                <slot />
+            </div>
+            
+        </div>
+        
+    </div>
 </template>
