a99e3d8092e631ed2409b6412e1bf55c1a02cacb
Ekkiddle <ekkiddle@gmail.com>
2024-02-28 23:31:01 -0700

    Beginnings of navbar and sidebar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-shrink-0 h-16 max-w-full z-10 bg-bone-50 border-b-2 border-red-200">
+    <div class="fixed flex flex-shrink-0 h-16 w-screen max-w-full z-10 bg-bone-100 border-b-2 border-red-200">
         <div class="flex h-16 items-center">
             <!-- Logo and Name (not in mobile view) + Menu Hamburger for the main side bar (mobile view)-->
             <nuxt-icon name="AlreadyDoneLogo" class="text-5xl text-amour-300 mx-3" />
-            <h1 class="font-museoModerno text-amour-200">
+            <h1 class="hidden md:block font-museoModerno text-amour-200">
                 Already Done
             </h1>
         </div>
@@ -11,9 +11,9 @@
             <!-- User points, Number of rewards redeemed, User name, Household name-->
             
         </div>
-        <div>
+        <div class="ml-auto items-center">
             <!-- Always accessable signout button in the corner (may remove later if I don't like here)-->
-            <v-btn @click="signOut()">
+            <v-btn @click="signOut()" class="m-3 h-10">
                 Sign Out
             </v-btn>
         </div>
---
+++ components/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="overflow-y-auto bg-sky w-16 md:w-64 h-screen">
+
+    </div>
+</template>
