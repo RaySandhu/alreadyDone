4877497f4bd06c58ddad277f247a04af34332ccc
Ekkiddle <ekkiddle@gmail.com>
2024-03-14 11:46:41 -0600

    Add link icons to side

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="fixed overflow-y-auto bg-amour-200 w-16 md:w-64 h-screen">
+    <div class="fixed overflow-y-auto bg-amour-200 w-16 md:w-64 h-screen pt-10 pl-1">
         <!-- Links -->
+        <div class="flex flex-col mt-10">
+            <!-- Dashboard -->
+            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
+                <nuxt-icon name="Apple" class="text-3xl mx-3" />
+                <h1 class="hidden md:block">Dashboard</h1>
+            </button>
+            
+            <!-- Household -->
+            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
+                <nuxt-icon name="House" class="text-3xl mx-3" />
+                <h1 class="hidden md:block">Household</h1>
+            </button>
+            <!-- Profile -->
+            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
+                <nuxt-icon name="Profile" class="text-3xl mx-3" />
+                <h1 class="hidden md:block">Profile</h1>
+            </button>
+            <!-- Settings -->
+            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
+                <nuxt-icon name="Settings" class="text-3xl mx-3" />
+                <h1 class="hidden md:block">Settings</h1>
+            </button>
+            <!-- Logout -->
+            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
+                <nuxt-icon name="Logout" class="text-3xl mx-3" />
+                <h1 class="hidden md:block">Logout</h1>
+            </button>
+        </div>
     </div>
 </template>
