5ecaca93cf11a41dc2352d3cb10b26bb56ddc71a
Ekkiddle <ekkiddle@gmail.com>
2024-03-14 14:37:07 -0600

    component for food panel created

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,32 +1,14 @@
 <script setup lang="ts">
-import FCard from '~/components/FCard.vue';
-import type { Food } from '~/server/utils/entityTypes';
-
-    const { data } = useAuth()
-
-    let tempFood:Food = {
-        'foodID' : 10,
-        'pointValue' : 10,
-        'quantity' : 1,
-        'hID' : 1,
-    };
 
 </script>
 
 <template>
-    <div class="p-2 bg-bone-50">
-        <h1>
+    <div class="p-5 bg-gradient-to-b from-bone-50 to-white">
+        <h1 class="font-museoModerno text-lg">
             Dashboard
         </h1>
         <!-- Panel for logging food/fruits. Admins have button to add and throw out/ delete items -->
-        <div class="my-8 w-full shadow-xl">
-            <div class = "px-2 bg-blue-300 h-10 rounded-tl-lg rounded-tr-lg">
-                Foods
-            </div>
-            <div class = "bg-blue h-64 w-full rounded-bl-lg rounded-br-lg flex flex-row">
-                <FCard :food="tempFood" />
-            </div>
-        </div>
+        <Food />
 
         <!-- Panel for redeeming rewards based on points. Admins have an option to create new rewards, activate/deactivate, delete -->
         <div class="my-8 bg-blue h-64 w-full rounded">
