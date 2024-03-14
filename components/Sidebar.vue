8e44abacd3f7507373259acc3035d5b61fb9f172
Ekkiddle <ekkiddle@gmail.com>
2024-03-14 14:33:05 -0600

    update to navbar and sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,33 +1,38 @@
+<script setup lang="ts">
+    const { signOut } = useAuth()
+
+</script>
+
 <template>
     <div class="fixed overflow-y-auto bg-amour-200 w-16 md:w-64 h-screen pt-10 pl-1">
         <!-- Links -->
         <div class="flex flex-col mt-10">
             <!-- Dashboard -->
-            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
-                <nuxt-icon name="Apple" class="text-3xl mx-3" />
-                <h1 class="hidden md:block">Dashboard</h1>
-            </button>
+            <v-btn class="flex justify-start align-center my-2" variant="text" color="white">
+                <nuxt-icon name="Apple" class="text-3xl" />
+                <h1 class="hidden md:block ml-1">Dashboard</h1>
+            </v-btn>
             
             <!-- Household -->
-            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
-                <nuxt-icon name="House" class="text-3xl mx-3" />
-                <h1 class="hidden md:block">Household</h1>
-            </button>
+            <v-btn class="flex justify-start align-center my-2" variant="text" color="white">
+                <nuxt-icon name="House" class="text-3xl" />
+                <h1 class="hidden md:block ml-1">Household</h1>
+            </v-btn>
             <!-- Profile -->
-            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
-                <nuxt-icon name="Profile" class="text-3xl mx-3" />
-                <h1 class="hidden md:block">Profile</h1>
-            </button>
+            <v-btn class="flex justify-start align-center my-2" variant="text" color="white">
+                <nuxt-icon name="Profile" class="text-3xl" />
+                <h1 class="hidden md:block ml-1">Profile</h1>
+            </v-btn>
             <!-- Settings -->
-            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
-                <nuxt-icon name="Settings" class="text-3xl mx-3" />
-                <h1 class="hidden md:block">Settings</h1>
-            </button>
+            <v-btn class="flex justify-start align-center my-2" variant="text" color="white">
+                <nuxt-icon name="Settings" class="text-3xl" />
+                <h1 class="hidden md:block ml-1">Settings</h1>
+            </v-btn>
             <!-- Logout -->
-            <button class="flex align-center text-bone-50 hover:text-amour-50 h-12">
-                <nuxt-icon name="Logout" class="text-3xl mx-3" />
-                <h1 class="hidden md:block">Logout</h1>
-            </button>
+            <v-btn class="flex justify-start align-center my-2" variant="text" color="white" @click="signOut()">
+                <nuxt-icon name="Logout" class="text-3xl" />
+                <h1 class="hidden md:block ml-1">Logout</h1>
+            </v-btn>
         </div>
     </div>
 </template>
