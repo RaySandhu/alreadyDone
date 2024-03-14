ed64446a03d0832cc09d765c889da62dd6bc8b05
Ekkiddle <ekkiddle@gmail.com>
2024-03-14 14:33:05 -0600

    update to navbar and sidebar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,26 +1,34 @@
 <template>
     <div class="fixed flex flex-shrink-0 h-16 w-screen max-w-full z-10 bg-gradient-to-b from-white to-orange-50 shadow-md border-b-2 border-red-200">
-        <div class="flex h-16 items-center">
+        <div class="flex h-16 items-center mr-1">
             <!-- Logo and Name (not in mobile view) + Menu Hamburger for the main side bar (mobile view)-->
             <nuxt-icon name="AlreadyDoneLogo" class="text-5xl text-amour-300 mx-3" />
-            <h1 class="hidden md:block font-museoModerno text-amour-200">
+            <h1 class="hidden md:block font-museoModerno text-amour-200 mr-20">
                 Already Done
             </h1>
         </div>
-        <div>
+        <div class="flex justify-center align-center">
             <!-- User points, Number of rewards redeemed, User name, Household name-->
+            <div class="flex align-center justify-center mx-5">
+                <nuxt-icon name="Points" class="text-2xl md:text-3xl text-gold-400 mt-1 mr-1" />
+                <h1 class="text-gold-400"> 500 </h1>
+            </div>
+            <div class="flex justify-center align-center mx-5">
+                <nuxt-icon name="Trophy" class="text-2xl md:text-2xl text-gold-400 mt-1 mr-1" />
+                <h1 class="text-gold-400"> 30 </h1>
+            </div>
             
         </div>
-        <div class="ml-auto items-center">
-            <!-- Always accessable signout button in the corner (may remove later if I don't like here)-->
-            <v-btn @click="signOut()" class="m-3 h-10">
-                Sign Out
-            </v-btn>
+        <div class="hidden md:block ml-auto mr-5">
+            <div class="flex flex-row justify-center align-center h-16">
+                <h1 class="text-amour-300 mx-5"> {{ data?.user?.name }} </h1>
+                <v-btn color="red"> Household 1 </v-btn>
+            </div>
         </div>
     </div>
 
 </template>
 
 <script setup lang="ts">
-    const { signOut } = useAuth()
+   const { data } = useAuth();
 </script>
